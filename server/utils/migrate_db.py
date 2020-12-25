33cfe824c76cd4fb3b4c58eb3ff98ec8d583870a
Parth Sarin <psarin@stanford.edu>
2020-12-25 13:58:32 -0600

    Add migration functions

---
+++ server/utils/migrate_db.py
@@ -0,0 +1,136 @@
+"""
+File: migrate_db.py
+-------------------
+
+Migrates the UnQLite database into a SQLite database.
+"""
+from unqlite import UnQLite
+import sqlite3
+import os
+from datetime import datetime
+
+UNQLITE_DB = 'stories.unqlite.db'
+source_db = UnQLite(UNQLITE_DB)
+SQLITE_DB = 'stories.db'
+
+class SQLTransaction:
+    def __init__(self, db_file):
+        self.conn = sqlite3.connect(db_file)
+    
+    def __enter__(self):
+        return self.conn.cursor()
+    
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.conn.commit()
+    
+    def close(self):
+        self.conn.close()
+
+
+try:
+    os.remove(SQLITE_DB)
+except FileNotFoundError:
+    pass
+
+
+db = SQLTransaction(SQLITE_DB)
+
+def build_schema():
+    with db as cursor:
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS Users (
+                id           INTEGER PRIMARY KEY,
+                display_name varchar(255),
+                slack_id     varchar(255),
+                first_name   varchar(255),
+                last_name    varchar(255)
+            );
+            """
+        )
+        
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS Stories (
+                id        INTEGER PRIMARY KEY,
+                max_lines int DEFAULT 10,
+                locked    bool DEFAULT FALSE,
+                locked_by int DEFAULT NULL,
+                locked_at timestamp DEFAULT NULL,
+            
+                FOREIGN KEY (locked_by) REFERENCES Users(id)
+            );
+            """
+        )
+
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS StoryLines (
+                id       INTEGER PRIMARY KEY,
+                author   int,
+                story    int,
+                line_idx int,
+                text     tinytext,
+
+                FOREIGN KEY (author) REFERENCES Users(id),
+                FOREIGN KEY (story) REFERENCES Stories(id)
+            );
+            """
+        )
+
+
+def migrate_users():
+    with source_db.transaction():
+        users = source_db.collection('users').all()
+    
+    with db as cursor:
+        for user in users:
+            cursor.execute(
+                """
+                INSERT INTO Users (display_name, slack_id, first_name, last_name)
+                VALUES (?, ?, ?, ?)
+                """,
+                (user['display_name'], user['slack_id'], user['first_name'], user['last_name'])
+            )
+
+
+def migrate_stories():
+    with source_db.transaction():
+        stories = source_db.collection('stories').all()
+    
+    with db as cursor:
+        for story in stories:
+            # First add to the stories
+            cursor.execute(
+                """
+                INSERT INTO Stories (max_lines, locked, locked_by, locked_at)
+                VALUES (?, ?, ?, ?);
+                """,
+                (story['max_lines'], story['locked'], story['locked_by'],
+                datetime.fromtimestamp(story['locked_at']) \
+                    if story['locked_at'] \
+                    else None)
+            )
+
+            # Next, add the lines
+            story_id = cursor.lastrowid
+            lines = story['lines']
+            for i, line in enumerate(lines):
+                cursor.execute(
+                    """
+                    INSERT INTO StoryLines (author, story, line_idx, text)
+                    VALUES (
+                        (SELECT id FROM Users WHERE display_name=?),
+                        ?,
+                        ?,
+                        ?
+                    );
+                    """,
+                    (line.get('author'), story_id, i, line['text'])
+                )
+
+
+if __name__ == '__main__':    
+    build_schema()
+    migrate_users()
+    migrate_stories()
---
+++ server/utils/sql_dump.sql
@@ -0,0 +1,86 @@
+-- SCHEMA
+CREATE TABLE IF NOT EXISTS Users (
+  id int AUTO_INCREMENT,
+  display_name varchar(255),
+  slack_id varchar(255),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Stories (
+  id int AUTO_INCREMENT,
+  max_lines int DEFAULT 10,
+  locked bool   DEFAULT FALSE,
+  locked_by int DEFAULT NULL,
+
+  FOREIGN KEY (locked_by) REFERENCES Users(id),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS StoryLines (
+  id int AUTO_INCREMENT,
+  author int,
+  story int,
+  line_idx int,
+  text tinytext,
+
+  FOREIGN KEY (author) REFERENCES Users(id),
+  FOREIGN KEY (story) REFERENCES Stories(id),
+  PRIMARY KEY (id)
+);
+
+
+-- Dummy data
+INSERT INTO Users (display_name, slack_id) VALUES (NULL, NULL);
+INSERT INTO Users (display_name, slack_id) VALUES ('Parth', '12345');
+INSERT INTO Users (display_name, slack_id) VALUES ('Kaya', '45678');
+
+INSERT INTO Stories (max_lines) VALUES (3);
+
+INSERT INTO StoryLines (author, story, line_idx, text) VALUES (
+    NULL,
+    1,
+    0,
+    'Mary had a little lamb whose fleece was white as wool.'
+);
+INSERT INTO StoryLines (author, story, line_idx, text) VALUES (
+    (SELECT id FROM Users WHERE display_name = 'Parth'),
+    1,
+    1,
+    'And everywhere that Mary went that lamb was sure to go.'
+);
+INSERT INTO StoryLines (author, story, line_idx, text) VALUES (
+    (SELECT id FROM Users WHERE display_name = 'Kaya'),
+    1,
+    2,
+    'Baaaaaa.'
+);
+
+--- Operations
+
+-- Get the last line of a story (this is actually wild)
+SELECT a.* 
+FROM StoryLines a
+LEFT OUTER JOIN StoryLines b
+  ON a.story = b.story AND a.line_idx < b.line_idx
+WHERE b.id is NULL AND a.story = 1;
+
+-- See which author locked the story
+SELECT b.*
+FROM Stories a
+LEFT OUTER JOIN Users b
+  ON a.locked_by = b.id
+WHERE a.locked AND a.id = 1;
+
+-- Get all lines of a story
+SELECT a.id, a.max_lines, b.line_idx, b.text, c.display_name, c.slack_id
+FROM Stories a
+LEFT OUTER JOIN StoryLines b
+  ON a.id = b.story
+LEFT OUTER JOIN Users c
+  ON b.author = c.id
+WHERE a.id = 1;
+
+-- Count number of lines written for a story
+SELECT COUNT(line_idx)
+FROM StoryLines
+WHERE story = 1;
